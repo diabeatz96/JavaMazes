64b6fa985148bec89e86c45b73c0f6e8a264b4b4
bigbl <adamkostandy@gmail.com>
2023-01-05 14:20:03 -0500

    Added new functions to Cell Class

--- src/Cell.java
+++ src/Cell.java
@@ -1,9 +1,10 @@
-import java.util.Map;
+import java.util.*;
 
 public class Cell {
     public Integer row;
     public Integer col;
-    public Cell north, south, east, left;
+    public Cell north, south, east, west;
+    public List<Cell> cellNeighbors;
     private Map<Cell, Boolean> links;
     public enum Direction {NORTH, SOUTH, EAST, WEST};
     private Map<Direction, Cell> neighbors;
@@ -15,6 +16,9 @@ public class Cell {
     public Cell(Integer row, Integer col) {
         this.row = row;
         this.col = col;
+        cellNeighbors = new ArrayList<Cell>();
+        links = new HashMap<Cell, Boolean>();
+        neighbors = new HashMap<Direction, Cell>();
     }
 
     public Cell link(Cell cellLink) {
@@ -27,5 +31,30 @@ public class Cell {
         links.remove(cellLink);
         cellLink.links.remove(this);
         return this;
+    }
+
+    public Set<Cell> links() {
+        return links.keySet();
+    }
+
+    public Boolean isLinked(Cell cell) {
+        return links.get(cell);
+    }
+
+    public List<Cell> neighbors() {
+        List<Cell> cellNeighbors = new ArrayList<Cell>();;
+        if(north != null){
+            cellNeighbors.add(north);
+        }
+        if(south != null){
+            cellNeighbors.add(south);
+        }
+        if(east != null){
+            cellNeighbors.add(east);
+        }
+        if(west != null){
+            cellNeighbors.add(west);
+        }
+        return cellNeighbors;
     }
 }
